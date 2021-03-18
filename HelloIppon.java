0639ac55015d6f70e16f6ae7db49191fb2548322
EC2 Default User <ec2-user@ip-172-31-82-127.ec2.internal>
2021-03-18 09:16:36 +0000

    file change

--- HelloIppon.java
+++ HelloIppon.java
@@ -2,7 +2,9 @@ public class HelloIppon
 {
 	public static void main (String [] args)
 	{
-		System.out.println("  ___ ____  ____   ___  _   _        __ _          _                     _        _                   _  ");
+		System.out.println("  ___ ____  ____   ___  _   _        __ _          _                     _        _                   _  ") System.out.println("  ___ ____  ____   ___  _   _        __ _          _                     _        _                   _  ");
+ System.out.println("  ___ ____  ____   ___  _   _        __ _          _                     _        _                   _  ");
+
 		System.out.println(" |_ _|  _ \\|  _ \\ / _ \\| \\ | |  _   / _(_)_ __ ___| |_    ___ ___  _ __ | |_ __ _(_)_ __   ___ _ __  | | ");
 		System.out.println("  | || |_) | |_) | | | |  \\| | (_) | |_| |  __/ __| __|  / __/ _ \\|  _ \\| __/ _` | |  _ \\ / _ \\  __| | | ");
 		System.out.println("  | ||  __/|  __/| |_| | |\\  |  _  |  _| | |  \\__ \\ |_  | (_| (_) | | | | || (_| | | | | |  __/ |    |_| ");
